09ee10321f517d395413f728c6494d1e95a448bd
David Grossman <dgrossman@coursera.org>
2020-11-06 18:03:43 +0100

    minimal reproducible example

--- examples/simple/i18n.js
+++ examples/simple/i18n.js
@@ -1,13 +1,13 @@
 const NextI18Next = require('next-i18next').default
 const { localeSubpaths } = require('next/config').default().publicRuntimeConfig
 const path = require('path')
+const I18nextCLILanguageDetector = require('./customDetector')
 
 module.exports = new NextI18Next({
   otherLanguages: ['de'],
   lng: 'en',
   localeSubpaths,
   localePath: path.resolve('./public/static/locales'),
-  shallowRender: true,
   debug: true,
   serverLanguageDetection: false,
   browserLanguageDetection: false,
