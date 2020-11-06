4adcc2e297e0ce0ef2e629c63e8f5d88fb7bcdaa
David Grossman <dgrossman@coursera.org>
2020-11-06 18:03:43 +0100

    minimal reproducible example

--- examples/simple/i18n.js
+++ examples/simple/i18n.js
@@ -1,9 +1,14 @@
 const NextI18Next = require('next-i18next').default
 const { localeSubpaths } = require('next/config').default().publicRuntimeConfig
 const path = require('path')
+const I18nextCLILanguageDetector = require('./customDetector')
 
 module.exports = new NextI18Next({
   otherLanguages: ['de'],
+  lng: 'en',
   localeSubpaths,
-  localePath: path.resolve('./public/static/locales')
+  localePath: path.resolve('./public/static/locales'),
+  debug: true,
+  serverLanguageDetection: false,
+  browserLanguageDetection: false,
 })
--- examples/simple/pages/index.js
+++ examples/simple/pages/index.js
@@ -1,10 +1,17 @@
 import PropTypes from 'prop-types'
+import {useState} from 'react'
 import { i18n, Link, withTranslation } from '../i18n'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
-const Homepage = ({ t }) => (
-  <>
+
+const Homepage = ({ t }) => {
+  const [state, setState] = useState('en');
+  if (state === 'en') {
+    i18n.changeLanguage('de', () => {setState('de')})
+  }
+
+  return i18n.language === 'de' && <>
     <main>
       <Header title={t('h1')} />
       <div>
