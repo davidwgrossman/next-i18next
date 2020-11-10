78beeeafe5da45ebc19a64da3c086cce8b987e8f
David Grossman <dgrossman@coursera.org>
2020-11-10 13:41:04 +0100

    customDetectors example

--- examples/simple/i18n.js
+++ examples/simple/i18n.js
@@ -1,9 +1,32 @@
 const NextI18Next = require('next-i18next').default
 const { localeSubpaths } = require('next/config').default().publicRuntimeConfig
 const path = require('path')
+const I18nextCLILanguageDetector = require('./customDetector')
+
+const detector = {
+  name: 'always-serve-german',
+  lookup: (req, res, options) => {
+    // You can add anything here (import custom files, etc.)
+    return 'de';
+  },
+  // This method is optional
+  cacheUserLanguage: (lng, options) => {
+    // options -> are passed in options
+    // lng -> current language, will be called after init and on changeLanguage
+
+    // store it
+  }
+};
+
 
 module.exports = new NextI18Next({
   otherLanguages: ['de'],
+  lng: 'en',
   localeSubpaths,
-  localePath: path.resolve('./public/static/locales')
+  localePath: path.resolve('./public/static/locales'),
+  debug: true,
+  detection: {
+    order: ['always-serve-german'],
+  },
+  customDetectors: [detector],
 })
